53de78c7712603a0c48a2459fd4f01870f321bfc
atbucchi <alisson.t.bucchi@gmail.com>
2024-06-24 13:02:16 +0100

    seção 9 a 14

--- cypress.config.js
+++ cypress.config.js
@@ -8,6 +8,7 @@ module.exports = defineConfig({
       hideCredentials: true,      //oculta token presente no Header
       requestMode: true,          //adiciona o comando cy.api (um "alias" para o comando cy.request) permanecendo ativo com o request.mode: true. 
     },
+    experimentalRunAllSpecs: true, 
   },
   fixturesFolder: false,
   video: false,  
